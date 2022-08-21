3f0e7dbb0018729da6f515941698258afbd4fb55
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-08-21 19:28:12 -0400

    XXX: workaround ncdu_2 being broken

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -2,5 +2,10 @@
 {
   nixpkgs.overlays = [
     (import ../support/overlay/overlay.nix)
+    (final: super: {
+      # ncdu2 build is broken
+      # https://github.com/NixOS/nixpkgs/issues/169461#issuecomment-1104425909
+      ncdu = super.ncdu_1;
+    })
   ];
 }
