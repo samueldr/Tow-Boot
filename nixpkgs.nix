91b0328b4e5b9b61088858ac7ea26fcaae0f5181
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-08-21 19:28:23 -0400

    nixpkgs.nix: Update pin

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
-  rev = "1905f5f2e55e0db0bb6244cfe62cb6c0dbda391d";
-  sha256 = "148f79hhya66qj8v5gn7bs6zrfjy1nbvdciyxdm4yd5p8r6ayzv6";
+  rev = "8ea014acc33da95ea56c902229957d8225005163";
+  sha256 = "1p3fv1zn3kf2r2nr0p6aqj7gns31krxhp0rsl8pfdmand7z5a5jd";
   tarball = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256;
